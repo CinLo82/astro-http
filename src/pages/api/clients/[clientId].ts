b78d2c1a56551e65d89060e601bf7e814a5d258f
Cintia Losada <cinlos3382@gmail.com>
2024-11-11 17:56:45 -0300

    crud: patch y delete

--- src/pages/api/clients/[clientId].ts
+++ src/pages/api/clients/[clientId].ts
@@ -1,6 +1,7 @@
-export const prerender = false;
-
+import { db, Clients, eq } from 'astro:db';
 import type { APIRoute } from "astro";
+
+export const prerender = false;
 
 // obtener la persona por el id
 export const GET: APIRoute = async ({ params, request }) => {
@@ -22,33 +23,68 @@ export const GET: APIRoute = async ({ params, request }) => {
 
 
 export const PATCH: APIRoute = async ({ params, request}) => {
-    const clientId = params.clientId
+    const clientId = params.clientId ?? '';
 
-    const body = {
-        Method: "PATCH",
-        "client_id": clientId
-    }
+    try {
+        const { id, ...body } = await request.json();
+        console.log(body)
 
-    return new Response(JSON.stringify(body), {
-        headers: {
-            "Content-Type": "application/json"
+        const results = await db
+            .update(Clients)
+            .set(body)
+            .where( eq(Clients.id, +clientId));
+
+        const updateClient = await db
+            .select()
+            .from(Clients)
+            .where( eq(Clients.id, +clientId));
+
+        return new Response(JSON.stringify(updateClient.at(0)), {
+            status: 201,
+            headers: {
+                'Content-Type': 'application/json'
             }
-        }
-    )
+        });
+    } catch (error) {
+        return new Response(JSON.stringify({ msg: "No body found" }), {
+            status: 400,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
 }
 
-export const DELETE: APIRoute = async ({ params, request}) => {
+export const DELETE: APIRoute = async ({ params, request }) => {
+    const clientId = params.clientId ?? '';
 
-    const clientId = params.clientId
+    try {
 
-    const body = {
-        Method: "DELETE",
-        "client_id": clientId
+        const { rowsAffected } = await db
+        .delete(Clients)
+        .where(eq(Clients.id, +clientId));
+    
+        if (rowsAffected > 0) {
+            return new Response(JSON.stringify({ msg: 'Deleted' }), {
+                status: 200,
+                headers: {
+                'Content-Type': 'application/json',
+                },
+            });
         }
-        return new Response(JSON.stringify(body), {
+        return new Response(JSON.stringify({ msg: 'Not found' }), {
+            status: 404,
             headers: {
-                "Content-Type": "application/json"
-            }
+                'Content-Type': 'application/json',
+            },
+        });
 
-        })
+    } catch (error) {
+        return new Response(JSON.stringify({ msg: "No body found" }), {
+            status: 400,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+            });
+        }
 }
