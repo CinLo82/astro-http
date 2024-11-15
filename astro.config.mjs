8e1529d387174e3fc73c332862881b2fc99074b9
Cintia Losada <cinlos3382@gmail.com>
2024-11-15 19:10:57 -0300

    eliminar el adaptador de Cloudflare de la configuración de Astro, agregar la tabla de Posts a la base de datos y actualizar el script de seed para incluir datos de publicaciones.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import db from '@astrojs/db';
-
-
-import cloudflare from '@astrojs/cloudflare';
+//import cloudflare from '@astrojs/cloudflare';
 
 
 // https://astro.build/config
@@ -13,5 +11,5 @@ export default defineConfig({
     site: 'https://example.com',
     integrations: [mdx(), sitemap(), db()],
     output: 'hybrid',
-    adapter: cloudflare(),
+    //adapter: cloudflare(),
 }); 
--- db/seed.ts
+++ db/seed.ts
@@ -1,4 +1,5 @@
-import { db, Clients } from 'astro:db';
+import { getCollection } from 'astro:content';
+import { db, Clients, Posts } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
@@ -8,5 +9,17 @@ export default async function seed() {
         { id: 3, name: "John", age: 53, isActive: true },
         { id: 4, name: "Jane", age: 27, isActive: false },
         { id: 5, name: "Kasim", age: 35, isActive: true },
-      ]);
+    ]);
+
+    const posts = await getCollection('blog');
+
+    await db.insert(Posts).values(
+        posts.map((p) => ({
+            id: p.id,
+            title: p.data.title,
+            likes: Math.round(Math.random() * 100),
+        }))
+    )
+       
+console.log('Seeded executed');
 }
